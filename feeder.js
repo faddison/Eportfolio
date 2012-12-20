4f79fe6570ad45d3025e666af7534ef52333d2d4
fraser <fraseraddison@gmail.com>
2012-12-20 00:02:30 -0800

    fixed asynchronous rss feed issue. graph now populates nodes correctly using blog posts from wordpress. had to use custom event firing function. fixed #6

--- feeder.js
+++ feeder.js
@@ -1,50 +1,63 @@
-function feeder(){
-
-var feedAddresses = [
-	"http://jtorn.wordpress.com/feed",
-	"http://fraseraddison.wordpress.com/feed",
-	"http://kmouse.wordpress.com/feed"];		
-	
-var count = 0;
-
-//google.load('feeds','1');
-google.load('feeds', '1', {callback : onGoogleReady});
-
-function onGoogleReady()
+function feeder()
 {
-	//var callback = populateRSSFeed('feed');
-	var callback = getFeed();
-	for (i = 0; i < feedAddresses.length; i++)
-	{
-		var address = feedAddresses[i];
-		console.log("Retrieving feed from "+address+" ...");
-		initializeRSSFeed(callback, address);
-	}
-}
 
-google.setOnLoadCallback(onGoogleReady);
-
-function initializeRSSFeed(callback, targetFeed) 
-{
-	var feed = new google.feeds.Feed(targetFeed);
-	feed.load(callback);
-}
-
-function getFeed()
-{
-	return function callback(result) 
-	{
-		if (!result.error)
-		{
-			if (count == 0);
-				addFeed(result.feed); 
-			console.log("Feed retrieved.");
-			count++;
-		}
-		else 
-			console.log("Feed retrieval failed!");
-		//console.log(JSON.stringify(feeds));
-	}
+	var feedAddresses = [
+		"http://jtorn.wordpress.com/feed",
+		"http://fraseraddison.wordpress.com/feed",
+		"http://kmouse.wordpress.com/feed"];		
 		
-}
+	var count = 0;
+
+	//google.load('feeds','1');
+	google.load('feeds', '1', {callback : onGoogleReady});
+
+	function onGoogleReady()
+	{
+		//var callback = populateRSSFeed('feed');
+		var callback = getFeed();
+		for (i = 0; i < feedAddresses.length; i++)
+		{
+			var address = feedAddresses[i];
+			console.log("Retrieving feed from "+address+" ...");
+			initializeRSSFeed(callback, address);
+		}
+	}
+
+	google.setOnLoadCallback(onGoogleReady);
+
+	function initializeRSSFeed(callback, targetFeed) 
+	{
+		var feed = new google.feeds.Feed(targetFeed);
+		feed.load(callback);
+	}
+
+	function getFeed()
+	{
+		return function callback(result) 
+		{
+			if (!result.error)
+			{
+				console.log("Feed retrieved.");
+				fireFeed(result.feed);
+			}
+			else 
+				console.log("Feed retrieval failed!");
+		}
+	}
+	
+	function fireFeed(feed)
+	{
+		//console.log(feed);
+		var event = new CustomEvent(
+		"newFeed",
+		{
+			detail: {
+				message: feed
+			},
+			bubbles: true,
+			cancelable: true
+		}
+		);
+		document.dispatchEvent(event);
+	}
 }
